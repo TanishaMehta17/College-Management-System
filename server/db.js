cacf0cae2343731e85efbdb2dd6bc707ad0be350
TanishaMehta17 <121849870+TanishaMehta17@users.noreply.github.com>
2024-08-18 08:51:58 +0530

    final changes

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Mohit@9893',
+    password: 'tanisha1709',
     database: 'dbms'
 });
 
